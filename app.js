7ae0975ad1d38d577b93df33e9b6f65b95b34ec6
mohdasaadeh <mass.1993@hotmail.com>
2022-05-08 16:59:14 +0300

    Configuring the app for production to work with React frontend

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
-if (process.env.NODE_ENV !== "production") {
-  require("dotenv").config();
-}
-
 const express = require("express");
 const mongoose = require("mongoose");
+const path = require("path");
 
 const app = express();
 mongoose.connect("mongodb://localhost:27017/brain-pocket", () => {
   console.log("Connected with MongoDB on port 27017 >>>");
 });
+
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+} else {
+  app.use(express.static("brain-pocket-frontend/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(
+      path.resolve(__dirname, "brain-pocket-frontend", "build", "index.html")
+    );
+  });
+}
 
 require("./services/cookieSession")(app);
 require("./services/passport")(app);
