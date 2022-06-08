0bb5fabfe08f1e154c47d5d26b8c12be1309fc48
mohdasaadeh <mass.1993@hotmail.com>
2022-06-08 14:57:28 +0300

    Fixing a bug in the list edit state and route

--- routes/main/lists/putList.js
+++ routes/main/lists/putList.js
@@ -1,3 +1,4 @@
+const ListRelation = require("../../../models/ListRelation");
 const deleteListFunc = require("./helpers/deleteListFunc");
 const postListFunc = require("./helpers/postListFunc");
 
@@ -6,9 +7,21 @@ const putList = async (req, res, next) => {
   const { id } = req.params;
   const userId = req.user.id;
 
-  await deleteListFunc(userId, id);
+  let listRelation = await ListRelation.findOne({ userId, _id: id }).populate(
+    "listId"
+  );
 
-  const listRelation = await postListFunc(list, userId);
+  for (let column in list) {
+    if (list[column] !== listRelation.listId[column]) {
+      await deleteListFunc(userId, id);
+
+      listRelation = await postListFunc(list, userId);
+
+      res.send(listRelation);
+
+      return;
+    }
+  }
 
   res.send(listRelation);
 };
